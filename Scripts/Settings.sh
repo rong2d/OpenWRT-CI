9dba328a5d20c2ddb7f04c1382eb6e329c01210e
rong2d <52213563+rong2d@users.noreply.github.com>
2024-05-19 09:41:58 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -5,9 +5,9 @@ sed -i "s/luci-theme-bootstrap/luci-theme-$WRT_THEME/g" $(find ./feeds/luci/coll
 
 CFG_FILE="./package/base-files/files/bin/config_generate"
 #修改默认IP地址
-sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $CFG_FILE
+#sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $CFG_FILE
 #修改默认主机名
-sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" $CFG_FILE
+#sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" $CFG_FILE
 #修改默认时区
 sed -i "s/timezone='.*'/timezone='CST-8'/g" $CFG_FILE
 sed -i "/timezone='.*'/a\\\t\t\set system.@system[-1].zonename='Asia/Shanghai'" $CFG_FILE
@@ -19,7 +19,7 @@ if [[ $WRT_URL == *"lede"* ]]; then
 	#添加编译日期标识
 	sed -i "s/(\(<%=pcdata(ver.luciversion)%>\))/\1 \/ $WRT_REPO-$WRT_DATE/" $LEDE_FILE
 	#修改默认WIFI名
-	sed -i "s/ssid=.*/ssid=$WRT_WIFI/g" ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
+	#sed -i "s/ssid=.*/ssid=$WRT_WIFI/g" ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
 elif [[ $WRT_URL == *"immortalwrt"* ]]; then
 	#添加编译日期标识
 	VER_FILE=$(find ./feeds/luci/modules/ -type f -name "10_system.js")
@@ -29,14 +29,14 @@ elif [[ $WRT_URL == *"immortalwrt"* ]]; then
 fi
 
 #配置文件修改
-echo "CONFIG_PACKAGE_luci-theme-$WRT_THEME=y" >> ./.config
-echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
+#echo "CONFIG_PACKAGE_luci-theme-$WRT_THEME=y" >> ./.config
+#echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
 
-if [[ $WRT_URL == *"lede"* ]]; then
-	echo "CONFIG_PACKAGE_luci-app-ssr-plus=y" >> ./.config
-	echo "CONFIG_PACKAGE_luci-app-openclash=y" >> ./.config
-elif [[ $WRT_URL == *"immortalwrt"* ]]; then
-	echo "CONFIG_PACKAGE_luci=y" >> ./.config
-	echo "CONFIG_LUCI_LANG_zh_Hans=y" >> ./.config
-	echo "CONFIG_PACKAGE_luci-app-homeproxy=y" >> ./.config
+#if [[ $WRT_URL == *"lede"* ]]; then
+#	echo "CONFIG_PACKAGE_luci-app-ssr-plus=y" >> ./.config
+#	echo "CONFIG_PACKAGE_luci-app-openclash=y" >> ./.config
+#elif [[ $WRT_URL == *"immortalwrt"* ]]; then
+#	echo "CONFIG_PACKAGE_luci=y" >> ./.config
+#	echo "CONFIG_LUCI_LANG_zh_Hans=y" >> ./.config
+#	echo "CONFIG_PACKAGE_luci-app-homeproxy=y" >> ./.config
 fi
